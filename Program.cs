af47a1ef252efdcdd449aadbf2373a272314b0b3
nella <147390743+prazakovaa@users.noreply.github.com>
2025-01-06 09:14:06 +0100

    Console

--- Program.cs
+++ Program.cs
@@ -5,5 +5,5 @@ using Coords;
 Coord c1 = new Coord(10, 10);
 Coord c2 = new Coord(10, 10);
 
-Console.WriteLine(c1 == c2); // False
+Console.WriteLine(c1 == c2); // True???
 Console.WriteLine(c2.Equals(c2)); // True
